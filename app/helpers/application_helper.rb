f6393c3f8479b38e01c3cea282f3a4fbb3ffb817
kumataro87 <371491s5@gmail.com>
2022-10-02 15:00:33 +0900

    create full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails"
+    if page_title
+      page_title + " | " + base_title
+    else  
+      base_title
+    end
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+
+  let(:base_title) {'Ruby on Rails'}
+
   describe "GET /home" do
     it "returns http success" do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "Home | #{base_title}"
     end
   end
-
+  
   describe "GET /help" do
     it "returns http success" do
       get "/static_pages/help"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "Help | #{base_title}"
     end
   end
 
@@ -19,6 +24,7 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get "/static_pages/about"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "About | #{base_title}"
     end
   end
 end
